c218a3177986d548c4a0394ecc422eef813b4dbf
jamesalexsmith <alex@getyolk.com>
2017-02-27 12:02:26 -0500

    Create a language folder, make cancellation of question work

--- src/flows/ask.js
+++ src/flows/ask.js
@@ -4,26 +4,64 @@ module.exports = (app) => {
   let slapp = app.slapp
   let smb = app.smb
 
-  slapp.command('/yolk', /^\s*ask\s*$/, (msg, text) => {
-    msg.route('ask_confirmation')
+  let lang_ask_question = require('../language/ask_question.json')
+  let lang_cancelled_question = require('../language/cancelled_question.json')
+
+  function slackAPI(token) {
+    return new app.SlackWebClient(token);
+  }
+
+  slapp.command('/yolk', 'test (.*)', (msg, text, question) => {
+    var slack = slackAPI(msg.meta.bot_token)
+    slack.channels.list(function(err, info) {
+       if (err) {
+           console.log('Error:', err);
+       } else {
+           for(var i in info.channels) {
+               console.log(info.channels[i]);
+           }
+       }
+    });
+    msg.respond('hi this is a test')
   })
 
-  slapp.message('(.*)', 'direct_mention', (msg, text) => {
+
+  slapp.command('/yolk', 'ask (.*)', (msg, text, question) => {
+    var state = {
+      asked_question: question
+    }
+    var response = lang_ask_question
+    response.attachments[0].title = question
+    response.attachments[0].callback_id = 'ask_callback'
+
     msg
-      .say('direct mentioned me')
-      .route('ask_confirmation')
+      .respond(response)
+      .route('ask_confirmation', state)
   })
 
-  slapp.message('(.*)', 'direct_message', (msg, text) => {
-    if (text === 'help'){
-      return {}
+
+  slapp.route('ask_confirmation', (msg, state) => {
+    if (msg.type !== 'action') {
+      msg
+        .respond('Please choose a Yes or No button :wink:')
+        .route('ask_confirmation', state)
+      return
     }
 
-    msg.say('TODO: search for similar questions')
-    msg.say('If you want to post this question direct mention me or use a /yolk command in the channel you want to post a question to!')
+    let answer = msg.body.actions[0].value
+
+    if (answer === 'cancel') {
+      var response = lang_cancelled_question
+      response.attachments[0].title = state.asked_question
+      msg.respond(response)
+    }
+
+    console.log(msg)
+    // msg.respond(msg.body.response_url, '')
   })
 
-  slapp.route('ask_confirmation', (msg) => {
+
+  slapp.route('add_details', (msg) => {
     // respond with a random entry from array
     msg.say(['Me too', 'Noted', 'That is interesting'])
   })
@@ -57,4 +95,20 @@ module.exports = (app) => {
 
 User asks question in DM
 Only searches, says to post this question direct mention me in the channel
+
+User direct mentions, take text
+search for questions with text
+button to ask see more questions
+button to say post this question
+
+confirmation to post to the channel
+
+start answer threading
+
 */
+
+
+
+
+
+
--- src/flows/search.js
+++ src/flows/search.js
@@ -3,5 +3,14 @@
 module.exports = (app) => {
   let slapp = app.slapp
 
+  slapp.message('(.*)', 'direct_message', (msg, text) => {
+    if (text === 'help'){
+      return {}
+    }
+
+    msg.say('TODO: search for similar questions')
+    msg.say('If you want to post this question direct mention me or use a /yolk command in the channel you want to post a question to!')
+  })
+
   return {}
 }
