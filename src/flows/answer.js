2d13d8b934e11055352d88ccc403d0a45e9ab95c
jamesalexsmith <alex@getyolk.com>
2017-03-02 16:08:12 -0500

    Get reactions working with answers

--- src/flows/answer.js
+++ src/flows/answer.js
@@ -3,5 +3,37 @@
 module.exports = (app) => {
   let slapp = app.slapp
 
+  slapp.message('(.*)', (msg, text) => {
+    if (msg.body.event.thread_ts !== undefined) {
+      if (msg.body.event.parent_user_id === msg.meta.bot_user_id) {
+        let token = msg.meta.bot_token
+        let timestamp = msg.body.event.ts
+        let channel = msg.body.event.channel
+
+        let thumbs_up = {
+          token: token,
+          timestamp: timestamp,
+          name: "+1",
+          channel: channel
+        }
+
+        let thumbs_down = {
+          token: token,
+          timestamp: timestamp,
+          name: "-1",
+          channel: channel
+        }
+
+        slapp.client.reactions.add(thumbs_up, (err) => {
+          if (err) console.log('Error adding reaction', err)
+        })
+
+        slapp.client.reactions.add(thumbs_down, (err) => {
+          if (err) console.log('Error adding reaction', err)
+        })
+      }
+    }
+  })
+
   return {}
 }
