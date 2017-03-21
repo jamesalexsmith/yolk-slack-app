6d844434e7adaa513ec09c637e25f8a0fd1c1092
jamesalexsmith <alex@getyolk.com>
2017-03-21 12:34:23 -0400

    Don't search when direct messaging in a thread

--- src/flows/search.js
+++ src/flows/search.js
@@ -8,7 +8,11 @@ module.exports = (app) => {
       return {}
     } else if (text.startsWith('Noticed you were asking a question!')) {
       return {}
+    } else if (msg.body.event.thread_ts) { // In a thread
+      return {}
     }
+
+    console.log(msg)
 
     msg.say('TODO: search for similar questions')
     msg.say('If you want to post this question direct mention me or use a /yolk command in the channel you want to post a question to!')
