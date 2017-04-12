d4d520ed9be696916678303dcdced73713396245
jamesalexsmith <alex@getyolk.com>
2017-04-12 15:45:07 -0400

    Refactor ask.js, remove details from question asking

--- src/flows/ask.js
+++ src/flows/ask.js
@@ -6,45 +6,23 @@ module.exports = (app) => {
 	let slapp = app.slapp
 	let smb = app.smb
 
-	let lang_ask_question = require('../language/post_question/ask_question.json')
-	let lang_ask_question_with_details = require('../language/post_question/ask_question_with_details.json')
+	let lang_ask_question_confirmation = require('../language/post_question/ask_question_confirmation.json')
 	let lang_cancelled_question = require('../language/post_question/cancelled_question.json')
-	let lang_add_details_question = require('../language/post_question/add_details_question.json')
-	let lang_question_with_details = require('../language/post_question/question_with_details.json')
-	let lang_question_without_details = require('../language/post_question/question_without_details.json')
+	let lang_post_question = require('../language/post_question/post_question.json')
 	let lang_notify_new_question = require('../language/notify_asker/new_question.json')
 
 	function slackAPI(token) {
 		return new app.SlackWebClient(token);
 	}
 
-	slapp.command('/yolk', 'ask (.*) %%% (.*)', (msg, text, question, details) => {
-		var state = {
-			question: question,
-			details: details
-		}
-		var response = lang_ask_question_with_details
-		response.attachments[0].title = question
-		response.attachments[0].callback_id = 'ask_callback'
-		response.attachments[0].fields[0].value = '```' + details + '```'
-
-		msg
-			.respond(response)
-			.route('ask_confirmation', state)
-
-	})
-
 	slapp.command('/yolk', 'ask (.*)', (msg, text, question) => {
-		var state = {
-			question: question,
-			details: null
-		}
-		var response = lang_ask_question
-		response.attachments[0].title = question
-		response.attachments[0].callback_id = 'ask_callback'
+		var state = {question: question}
+		var reply = lang_ask_question_confirmation
+		reply.attachments[0].title = question
+		reply.attachments[0].callback_id = 'ask_callback'
 
 		msg
-			.respond(response)
+			.respond(reply)
 			.route('ask_confirmation', state)
 	})
 
@@ -54,31 +32,14 @@ module.exports = (app) => {
 		if (answer === 'cancel') {
 			var reply = lang_cancelled_question
 			reply.attachments[0].title = state.question
-			if (state.details !== null) {
-				reply.attachments[0].fields[0].value = state.details
-			}
-
 			msg.respond(reply)
 			return
 		}
-		
-		else if (answer === 'details') {
-			var reply = lang_add_details_question
-			reply.attachments[0].title = 'Add details by typing this in the channel you wish to ask the question in'
-			reply.attachments[0].fields[0].value = '```/yolk ask ' + state.question + ' %%% [YOUR_DETAILS_HERE]```'
-			msg.respond(reply)
-		} 
-		
+
 		else if (answer === 'post') {
 			var userMention = '<@' + msg.body.user.name + '|' + msg.body.user.id + '>'
 			var channelMention = '<!channel|channel>'
-
-			if (state.details !== null) {
-				var reply = lang_question_with_details
-				reply.attachments[0].fields[0].value = state.details
-			} else {
-				var reply = lang_question_without_details
-			}
+			var reply = lang_post_question
 
 			msg.respond({
 				delete_original: true
@@ -86,7 +47,6 @@ module.exports = (app) => {
 
 			reply.text = ''
 			reply.attachments[0].title = 'Hey ' + channelMention + ', ' + userMention + ' has a question: \n ' + state.question
-			reply.attachments[0].callback_id = 'ask_callback'
 			reply.attachments[0].ts = Math.floor(new Date() / 1000)
 
 
@@ -151,10 +111,6 @@ module.exports = (app) => {
 			});
 
 		}
-	})
-
-	slapp.action('ask_callback', 'answer', (msg, text) => {
-		console.log(msg)
 	})
 
 	return {}
