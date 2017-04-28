e37cfa0722943f32fd870d3971b4d50580b08163
jamesalexsmith <alex@getyolk.com>
2017-04-28 13:31:59 -0400

    Start working on search with watson

--- src/flows/search.js
+++ src/flows/search.js
@@ -8,7 +8,7 @@ module.exports = (app) => {
 		msg.respond('Searching.')
 	})
 
-	slapp.message('(.*)', ['direct_message', 'direct_mention'], (msg, text) => {
+	slapp.message('(.*)', ['direct_message'], (msg, text) => {
 		if (text === 'help') {
 			return {}
 		} else if (text.startsWith('Noticed you asked me to post a ')) {
@@ -17,43 +17,47 @@ module.exports = (app) => {
 			return {}
 		}
 
-		let searchQuery = 'from:@yolk Hey @channel has a question: ' + text
-
-		let searchOptions = {
-			token: msg.meta.app_token,
-			query: searchQuery,
-			count: 100
-		}
-
-		slapp.client.search.messages(searchOptions, (err, searchData) => {
-			if (err) {
-				console.log('Error searching for yolk', err)
-				return
-			}
-
-			// Filter out messages that aren't threaded (hack to find yolk things)
-			let messages = []
-			for (var i = 0; i < searchData.messages.matches.length; i++) {
-				if (searchData.messages.matches[i].permalink.includes('thread_ts')) {
-					messages.push(searchData.messages.matches[i])
-				}
-			}
-
-			if (messages.length === 0) {
-				msg.say('No results found!')
-				return
-			}
-
-			let reply = 'Found ' + messages.length + ' matches!'
-			for (var i = 0; i < messages.length; i++) {
-				reply += ' \n'
-				reply += messages[i].permalink
-			}
-			msg.say(reply)
-		})
+		// Found matches?
+			// hyper link q&a threads
+			// accept answer?
+				// send thanks
+				// upload data to DB validating q&a
+				// return
+			// next page? // TODO confidence score
+				// loop back
+			// Post question?
+				// post the question
+			// return
 	})
 
+	function confirmPostYolkQuestion(msg) {
+		
+	}
 
+	function postQuestion(msg) {
+		
+	}
+
+	function paginateMatches(qaMatches, paginationLength) {
+		// return pagination
+	}
+
+
+	function nextPageExists(paginatedQAMatches, pageIndex) {
+
+	}
+
+	function prevPageExists(paginatedQAMatches, pageIndex) {
+
+	}
+
+	function sendValidationData(msg, qaMatch) {
+		// send thanks
+	}
+
+	function sendThanks(msg, qaMatch) {
+		
+	}
 
 	return {}
 }
