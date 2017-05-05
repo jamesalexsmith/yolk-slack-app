fecf77cb715b8c9b3a302cdad87682461d3e8961
jamesalexsmith <alex@getyolk.com>
2017-05-05 09:19:53 -0400

    Refactor search flow

--- src/flows/search.js
+++ src/flows/search.js
@@ -25,6 +25,7 @@ module.exports = (app) => {
 		}
 
 		db.Question.find({$text: {$search: text}, answered: true})
+			// TODO ORDER BY RELEVANCE
 			.limit(9)
 			.exec(function (err, docs) {
 				if (err) {
@@ -38,9 +39,17 @@ module.exports = (app) => {
 				} else {
 					// Found already matched Q&A pairs
 					let paginations = paginateMatches(docs)
-					let reply = formatResults(msg, paginations[0])
-					reply.attachments.push(generatePaginationNavigation(paginations, 0))
-					msg.say(reply)
+					let reply = formatResults(msg, paginations, 0)
+					
+					let state = {
+						'reply': reply,
+						'paginations': paginations,
+						'index': 0,
+					}
+
+					msg
+						.say(reply)
+						.route('search_flow', state)
 				}
 			});
 
@@ -66,8 +75,9 @@ module.exports = (app) => {
 		return paginations
 	}
 
-	function formatResults(msg, pagination) {
-		let results = lang_results
+	function formatPagination(msg, pagination) {
+		// Render all the qa pairs in the pagination
+		let formatted_pagination = []
 		let formatted_qa_pair = lang_qa_pair
 		for (var i = 0; i < pagination.length; i++) {
 			let qa_pair = pagination[i]
@@ -75,9 +85,21 @@ module.exports = (app) => {
 			formatted_qa_pair.title = 'Q: ' + qa_pair.question + '\nA: ' + qa_pair.latest_accepted_answer
 			formatted_qa_pair.title_link = generateLinkToFirstComment(msg, qa_pair)
 			formatted_qa_pair.footer = generateFooter(qa_pair.latest_accepted_user_id, qa_pair.answered_at)
-			results.attachments.push(JSON.parse(JSON.stringify(formatted_qa_pair)))
+			formatted_pagination.push(JSON.parse(JSON.stringify(formatted_qa_pair)))
 		}
-		console.log(results)
+		return formatted_pagination
+	}
+
+	function formatResults(msg, paginations, pagination_index) {
+		// For each paginatation format their qa pairs
+		let formattedPaginations = []
+		paginations.forEach(function(pagination) {
+			formattedPaginations.push(formatPagination(msg, pagination))
+		}, this);
+
+		let results = lang_results
+		results.attachments = formattedPaginations[pagination_index]
+		results.attachments.push(generatePaginationNavigation(paginations, pagination_index))
 		return results
 	}
 
