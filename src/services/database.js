28397342fc784d60a80d74f97f9162f52efdebfb
jamesalexsmith <alex@getyolk.com>
2017-05-10 17:28:22 -0400

    Create user model and db methods

--- src/services/database.js
+++ src/services/database.js
@@ -51,7 +51,7 @@ module.exports = (mongoose) => {
 
     validationSchema.index({'$**': 'text'});
 
-    var teamsSchema = new Schema({
+    var teamSchema = new Schema({
         team_name: String,
         team_id: String,
         paid: Boolean,
@@ -61,16 +61,28 @@ module.exports = (mongoose) => {
         added_at: Date,
     })
 
+    var userSchema = new Schema({
+        user_id: String,
+        user_name: String,
+        team_name: String,
+        team_id: String,
+        onboarded: Boolean,
+        onboarded_at: Date,
+        google_token: String
+    })
+
     // Models
     var Question = mongoose.model('Question', questionSchema)
     var Validation = mongoose.model('Validation', validationSchema)
-    var Team = mongoose.model('Team', teamsSchema)
+    var Team = mongoose.model('Team', teamSchema)
+    var User = mongoose.model('User', userSchema)
 
     // Methods
     var methods = {}
     methods.Question = Question
     methods.Validation = Validation
     methods.Team = Team
+    methods.User = User
 
     methods.saveTeam = function(contents) {
         let team = new Team(contents)
@@ -85,6 +97,23 @@ module.exports = (mongoose) => {
         Team.findOneAndUpdate(query, update, function (err, doc) {
             if (err) {
                 console.log('Error updating team in mongodb', err)
+            }
+        })
+    }
+
+    methods.saveUser = function(contents) {
+        let user = new User(contents)
+        user.save(function (err, user) {
+            if (err) {
+                console.log('Error saving user', err)
+            } 
+        })
+    }
+
+    methods.updateUser = function(query, update) {
+        User.findOneAndUpdate(query, update, function (err, doc) {
+            if (err) {
+                console.log('Error updating user in mongodb', err)
             }
         })
     }
