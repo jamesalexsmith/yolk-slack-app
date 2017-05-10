5b07609552372b378649365f11ab7ac3eb0fe135
jamesalexsmith <alex@getyolk.com>
2017-05-10 16:35:25 -0400

    Beautify files

--- src/flows/team_events.js
+++ src/flows/team_events.js
@@ -1,23 +1,26 @@
 'use strict'
 
 module.exports = (app) => {
-  let db = app.db
-  let slapp = app.slapp
+	let db = app.db
+	let slapp = app.slapp
 
-  slapp.event('bb.team_added', function (msg) {
-    let teamContents = {
-        team_name: msg.meta.team_name,
-        team_id: msg.meta.team_id,
-        paid: false,
-        paid_at: null,
-        launched: false,
-        launched_at: null,
-        added_at: Date(),
-    }
-    
-    db.saveTeam(teamContents)
-  })
+	slapp.event('bb.team_added', function (msg) {
+		let teamContents = {
+			team_name: msg.meta.team_name,
+			team_id: msg.meta.team_id,
+			paid: false,
+			paid_at: null,
+			launched: false,
+			launched_at: null,
+			added_at: Date(),
+		}
 
-  return {}
+		db.saveTeam(teamContents)
+	})
+
+	slapp.command('/yolk', 'help', (msg, text, question, details) => {
+		msg.respond(help)
+	})
+
+	return {}
 }
-
