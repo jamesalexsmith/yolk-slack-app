18e813bd959b0636772951f5d91398116ded9873
jamesalexsmith <alex@getyolk.com>
2017-05-10 18:16:46 -0400

    Msg all users when launching

--- src/flows/team_events.js
+++ src/flows/team_events.js
@@ -24,7 +24,51 @@ module.exports = (app) => {
 		let updateQuery = {$set: {'launched': true, 'launched_at': new Date()}}
 		db.updateTeam(teamQuery, updateQuery)
 		msg.say('Launching Yolk to the rest of your Slack!')
+
+		startOnboardingUsers(msg)
 	})
+
+	function startOnboardingUsers(msg) {
+		// Fetch all users
+		let usersOptions = {token: msg.meta.bot_token}
+		slapp.client.users.list(usersOptions, (err, usersData) => {
+			if (err) {
+				console.log('Error listing all users in launch flow', err)
+				return
+			}
+
+			for (var i = 0; i < usersData.members.length; i++) {
+				let user = usersData.members[i]
+
+				if (! (user.is_bot || user.deleted)) {
+					// Open IM with each user
+					let imOptions = {
+						token: msg.meta.bot_token,
+						user: user.id
+					}
+					slapp.client.im.open(imOptions, (err, imData) => {
+						if (err) {
+							console.log('Error opening im with user for onboarding', err)
+						}
+
+						// Display onboarding message
+						let msgOptions = {
+							token: msg.meta.bot_token,
+							channel: imData.channel.id,
+							text: "THIS IS A LAUNCH TEST"
+						}
+
+						slapp.client.chat.postMessage(msgOptions, (err, postData) => {
+							if (err) {
+								console.log('Error sending onboarding dm', err)
+							}
+						})
+					})
+				}
+			}
+		})
+		
+	}
 
 	return {}
 }
