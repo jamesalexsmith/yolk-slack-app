734b15f044e35823355d0b856a104de1f33dc6d5
jamesalexsmith <alex@getyolk.com>
2017-05-31 21:24:54 -0400

    More more testing live

--- src/flows/import.js
+++ src/flows/import.js
@@ -34,15 +34,27 @@ module.exports = (app) => {
 		let team_id = msg.body.team_id
 		let team_name = msg.meta.team_name
 		let bot_user_id = msg.meta.bot_user_id
-		console.log('Channel:', channel)
-		console.log('Channel Matches:', channel.match('<#(.*)\\|'))
-		let channel_id = channel.match('<#(.*)\\|')[1]
 
+		slapp.client.channels.list({token: msg.meta.bot_token}, (err, data) => {
+			if (err) {
+				console.log('Error fetching channels list in import', data)
+				msg.respond('Can\'t list channels for this slack...')
+				return
+			}
 
-		let contents = createQuestionModelContents(question, answer, team_id, team_name, bot_user_id, channel_id)
-		db.saveQuestion(contents)
+			for (var i = 0; i < data.channels.length; i++) {
+				if (channel == data.channels[i].name) {
+					let channel_id = data.channels[i].id
+					let contents = createQuestionModelContents(question, answer, team_id, team_name, bot_user_id, channel_id)
+					db.saveQuestion(contents)
 
-		msg.say('>Q: ' + question + '\n>A: ' + answer + '\n Saved to DB in channel: <#' + channel_id + '>')
+					msg.say('>Q: ' + question + '\n>A: ' + answer + '\n _Saved to DB in channel: <#' + channel_id + '>_')
+					return
+				}	
+			}
+			msg.say('_Could not find the channel, please type it again correctly..._')
+		})
+
 	})
 
 	// slapp.event('file_shared', function (msg) {
